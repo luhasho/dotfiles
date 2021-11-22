178a989b303f415a3955f217b974bae30900c62a
luhasho <gogo777god@gmail.com>
2021-11-23 03:50:10 +0900

    fix

--- setlink.bat
+++ setlink.bat
@@ -1,6 +1,10 @@
 rem シンボリックリンクを作成する
 mklink %HOMEPATH%\_vimrc %HOMEPATH%\dotfiles\_vimrc
 mklink %HOMEPATH%\_gvimrc %HOMEPATH%\dotfiles\_gvimrc
+
+rem PowerShell関連
+mklink %HOMEPATH%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 %HOMEPATH%\dotfiles\Microsoft.PowerShell_profile.ps1
+
 rem bash関連
 mklink %HOMEPATH%\.bash_profile %HOMEPATH%\dotfiles\.bash_profile
 mklink %HOMEPATH%\.bashrc %HOMEPATH%\dotfiles\.bashrc
